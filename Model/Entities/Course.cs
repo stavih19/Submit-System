4e4e224664d0a273742f6777c80ed5e744909cc5
napan11 <46086872+napan11@users.noreply.github.com>
2021-05-15 23:12:35 +0300

    courses update
    courses update

--- Model/Entities/Course.cs
+++ Model/Entities/Course.cs
@@ -1,10 +1,24 @@
 using System.Collections.Generic;
 namespace Submit_System {
     public class Course {
-        public string Name {get; set; }
-        public int Number {get; set; }
+
+        public Course(){}
+        public Course(string id,int number,string name,int year,int semester){
+            this.ID = id;
+            this.Number = number;
+            this.Name = name;
+            this.Year =year;
+            this.Semester = semester;
+        }
         public string ID {get; set; }
+
+        public int Number {get; set; }
+        
+        public string Name {get; set; }
+        
         public int Year { get; set; }
+
+        public int Semester{ get; set; }
 
     }
 }
