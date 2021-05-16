786887e3d0ed974fb5cc4ba9ca9bf0d9b7852a8d
napan11 <46086872+napan11@users.noreply.github.com>
2021-05-16 16:59:56 +0300

    Add files via upload

--- Archtechture files/creation.sql
+++ Archtechture files/creation.sql
@@ -2,50 +2,51 @@ CREATE DATABASE Submit02;
 GO
 USE Submit02;
 CREATE TABLE Users (
-    user_id varchar(32),
+    user_id varchar(10),
     password_hash varchar(32),
     name varchar(32),
     email varchar(64)
 );
 CREATE TABLE Courses (
-    course_id varchar(32),
+    course_id varchar(16),
     course_number int,
     course_name varchar(32),
     year int,
     semester int 
 );
 CREATE TABLE Student_Course(
-    user_id varchar(32),
-    course_id varchar(32)
+    user_id varchar(10),
+    course_id varchar(16)
 );
 CREATE TABLE Metargel_Course(
-    user_id varchar(32),
-    course_id varchar(32)
+    user_id varchar(10),
+    course_id varchar(16)
 );
 CREATE TABLE Exercise(
-    exercise_id varchar(32),
+    exercise_id varchar(48),
     exercise_name varchar(32),
-    course_id varchar(32),
-    original_exercise_id varchar(32),
+    course_id varchar(16),
+    original_exercise_id varchar(48),
     max_submitters int,
     test_files_location varchar(64),
     late_submittion_settings varchar(64),
     programming_language varchar(32),
     auto_test_grade_value int,
     style_test_grade_value int,
+    is_active int
 );
 CREATE TABLE Checker_Exercise(
-    user_id varchar(32),
-    exercise_id varchar(32)
+    user_id varchar(10),
+    exercise_id varchar(48)
 );
 CREATE TABLE Submission_Dates(
-    exercise_id varchar(32),
+    exercise_id varchar(48),
     submission_date_id int,
     submission_date DATE
 );
 CREATE TABLE Submission(
     submission_id varchar(32),
-    exercise_id varchar(32),
+    exercise_id varchar(48),
     files_location varchar(64),
     auto_grade int,
     style_grade int,
@@ -58,7 +59,7 @@ CREATE TABLE Submission(
     chat_appeal_id varchar(32)
 );
 CREATE TABLE Submitters(
-    user_id varchar(32),
+    user_id varchar(10),
     submission_id varchar(32),
     submitter_type int
 );
@@ -74,9 +75,9 @@ CREATE TABLE Massage(
     massage_time DATETIME,
     massage_type int,
     massage_value varchar(255),
-    sender_user_id varchar(32),
+    sender_user_id varchar(10),
     massage_status int,
-    course_id varchar(32),
+    course_id varchar(16),
 );
 CREATE TABLE Managers(
     id varchar(32),
