6d09b65b7ff29ffbbf3aed28f1b3b54efb43d325
Hagai Stavi <stavih19@gmail.com>
2021-07-26 22:13:58 +0300

    Update creation.sql

--- Archtechture files/creation.sql
+++ Archtechture files/creation.sql
@@ -2,53 +2,32 @@ CREATE DATABASE Submit02;
 GO
 USE Submit02;
 CREATE TABLE Users (
-    user_id nvarchar(10),
-    password_hash nvarchar(48),
-    name nvarchar(32),
-    email nvarchar(64)
     user_id varchar(10),
     password_hash varchar(48),
     name varchar(32),
     email varchar(64)
 );
 CREATE TABLE Courses (
-    course_id nvarchar(16),
     course_id varchar(16),
     course_number int,
-    course_name nvarchar(32),
     course_name varchar(32),
     year int,
     semester int 
 );
 CREATE TABLE Student_Course(
-    user_id nvarchar(10),
-    course_id nvarchar(16)
     user_id varchar(10),
     course_id varchar(16)
 );
 CREATE TABLE Metargel_Course(
-    user_id nvarchar(10),
-    course_id nvarchar(16)
-);
-CREATE TABLE Checker_Course(
-    user_id nvarchar(10),
-    course_id nvarchar(16)
     user_id varchar(10),
     course_id varchar(16)
 );
 CREATE TABLE Exercise(
-    exercise_id nvarchar(48),
-    exercise_name nvarchar(32),
-    course_id nvarchar(16),
-    original_exercise_id nvarchar(48),
     exercise_id varchar(48),
     exercise_name varchar(32),
     course_id varchar(16),
     original_exercise_id varchar(48),
     max_submitters int,
-    test_files_location nvarchar(64),
-    late_submittion_settings nvarchar(64),
-    programming_language nvarchar(32),
     test_files_location varchar(64),
     late_submittion_settings varchar(64),
     programming_language varchar(32),
@@ -58,52 +37,35 @@ CREATE TABLE Exercise(
     multiple_submissions int
 );
 CREATE TABLE Checker_Exercise(
-    user_id nvarchar(10),
-    exercise_id nvarchar(48)
     user_id varchar(10),
     exercise_id varchar(48)
 );
-
 CREATE TABLE Submission_Dates(
-    exercise_id nvarchar(48),
     exercise_id varchar(48),
     submission_date_id int,
-    reduction int,
-    [group] int,
     submission_date DATE
 );
 CREATE TABLE Submission(
-    submission_id nvarchar(60),
-    exercise_id nvarchar(48),
-    files_location nvarchar(64),
     submission_id varchar(60),
     exercise_id varchar(48),
     files_location varchar(64),
     auto_grade int,
     style_grade int,
     manual_final_grade int,
-    manual_check_data nvarchar(255),
     manual_check_data varchar(255),
     submission_status int,
     submission_date_id int,
     time_submitted DATETIME,
-    chat_late_id nvarchar(32),
-    chat_appeal_id nvarchar(32)
     chat_late_id varchar(32),
     chat_appeal_id varchar(32)
 );
 CREATE TABLE Submitters(
-    user_id nvarchar(10),
-    submission_id nvarchar(60),
     user_id varchar(10),
     submission_id varchar(60),
     submitter_type int,
-    exercise_id nvarchar(48)
     exercise_id varchar(48)
 );
 CREATE TABLE Chat(
-    chat_id nvarchar(60),
-    submission_id nvarchar(60),
     chat_id varchar(60),
     submission_id varchar(60),
     chat_status int,
@@ -114,38 +76,12 @@ CREATE TABLE Message(
     chat_id varchar(60),
     message_time DATETIME,
     message_type int,
-    message_value nvarchar(8000),
-    sender_user_id nvarchar(10),
     message_value varchar(255),
     sender_user_id varchar(10),
     message_status int,
-    course_id nvarchar(16),
-    from_teacher int,
-    sender_name nvarchar(64),
     course_id varchar(16)
 );
 CREATE TABLE Managers(
-    id nvarchar(32),
-    password_hash nvarchar(64)
-);
-
-CREATE TABLE Test(
-    [weight] int,
-    input ntext,
-    expected_output ntext,
-    output_file_name nvarchar(32),
-    arguments_string ntext,
-    timeout_in_seconds int,
-    main_sourse_file nvarchar(32),
-    adittional_files_location nvarchar(64),
-    exercise_id nvarchar(48),
-    [type] int
-);
-
-CREATE TABLE Tokens(
-    user_id varchar(10),
-    token varchar(64),
-    creation_date DATE
     id varchar(32),
     password_hash varchar(48)
 );
