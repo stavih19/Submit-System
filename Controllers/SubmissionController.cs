36928270bee2a45369ffe1afbe39ba68103e09a8
napan11 <46086872+napan11@users.noreply.github.com>
2021-07-28 15:17:49 +0300

    Update SubmissionController.cs
    david's changes

--- Controllers/SubmissionController.cs
+++ Controllers/SubmissionController.cs
@@ -5,49 +5,43 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using System.Net;
 using System.Diagnostics;
 using System.Net.Mime;
 using Microsoft.AspNetCore.StaticFiles;
+using System.IO;
 
 namespace Submit_System.Controllers
 {
     [ServiceFilter(typeof(AuthFilter))]
     public class SubmissionController : AbstractController
     {
-
-        private readonly ILogger<ExerciseController> _logger;
-
         private readonly DatabaseAccess _access;
+        private readonly MossClient _client;
 
-        public SubmissionController(ILogger<ExerciseController> logger, DatabaseAccess access)
+        public SubmissionController(DatabaseAccess access, MossClient client)
         {
-            _logger = logger;
             _access = access;
+            _client = client;
         }
 
         [Route("Student/GradesList")]
         [HttpGet]
         public ActionResult<List<ExerciseGradeDisplay>> GetStudentGrades(string userid)
         {
-            return _access.GetStudentGrades(userid);
+            return HandleDatabaseOutput(_access.GetStudentGrades(userid));
         }
         [Route("Student/SubmissionDetails")]
         [HttpGet]
         public ActionResult<StudentExInfo> GetSubmission(string userid, string exerciseId)
         {
             var a = _access.GetStudentSubmission(userid, exerciseId);
-            if(a == null)
-            {
-                return NotFound("Exercise Not found");
-;           }
-            return a;
+            return HandleDatabaseOutput(a);
         }
         [Route("Student/MessageList")]
         [HttpGet]
         public ActionResult<List<Message>> GetLateMessages(string userid, string chatId)
         {
-            return _access.GetMesssages(userid, chatId);
+            return HandleDatabaseOutput(_access.GetMesssages(userid, chatId, Role.Student));
         }
 
         [Route("Student/RunResult")]
@@ -60,17 +54,25 @@ namespace Submit_System.Controllers
         [HttpPost]        
         public ActionResult PostMessage(string userid, string chatId, [FromBody] string msg)
         {
-            _access.InsertMessage(userid, chatId, msg);
-            return new OkResult();
+            return HandleDatabaseOutput(_access.InsertStudentMessage(userid, chatId, msg));
+        }
+        [Route("Student/Appeal")]
+        [HttpPost]        
+        public ActionResult Appeal(string userid, string submissionid, [FromBody] MessageInput msg)
+        {
+            return HandleDatabaseOutput(_access.Appeal(userid, submissionid, msg));
+        }
+        [Route("Student/ExtensionRequest")]
+        [HttpPost]        
+        public ActionResult Extension(string userid, string submissionid, [FromBody] MessageInput msg)
+        {
+            return HandleDatabaseOutput(_access.Appeal(userid, submissionid, msg));
         }
         [Route("Student/SubmitExercise")]
         [HttpPost]        
-        public ActionResult<SubmitResult> Submit(string userid, string exerciseId, [FromBody] List<UploadedFile> files)
+        public ActionResult<SubmitResult> Submit(string userid, string exerciseId, [FromBody] List<SubmitFile> files, bool final)
         {
-            if(files == null) {
-                return BadRequest("No files");
-            }
-            if(!files.Any())
+            if(!files?.Any() ?? false || exerciseId == null)
             {
                 return BadRequest("No files");
             }
@@ -81,57 +83,49 @@ namespace Submit_System.Controllers
                     return BadRequest("No files");
                 }
             }
-            if(exerciseId == null || exerciseId == "")
-            {
-                return BadRequest("No exercise ID");
-            }
-            string path = _access.CreateDirectoryPath(userid, exerciseId);
-            if(path == null)
-            {
-                return NotFound("Exercise not found");
-            }
+            (string path, string subid, DBCode code) =  _access.CreateSubmissionPath(userid, exerciseId);
+            if(path == null) { return NotFound("Exercise not found"); }
+            List<string> submittedFiles;
             try
             {
-                FileUtils.StoreFiles(files, path);
+                submittedFiles = FileUtils.SubmitFiles(files, path);
             }
-            catch(ArgumentException e)
+            catch(System.Security.SecurityException e)
             {
                 return BadRequest(e.Message);
             }
             catch (Exception e)
             {
-                Trace.WriteLine(e.ToString());
+                Debug.WriteLine(e.ToString());
                 return ServerError("There was an issue with uploading the files");
             }
-            var filenames = System.IO.Directory.GetFiles(path, "*", System.IO.SearchOption.AllDirectories);
-            for(int i = 0; i < filenames.Length; i++)
+            if(final)
             {
-                filenames[i] = FileUtils.GetRelativePath(filenames[i], path);
+                _access.MarkSubmitted(subid, userid);
             }
             return new SubmitResult {
                 Message = $"Exercise Submitted successfully.\n Date Submitted: {DateTime.Now.ToString("m/dd/yyyy")}",
-                Files = filenames
+                Files = submittedFiles
             };
         }
         [HttpGet]
         [Route("Checker/SubmissionListToCheck")]
-        public ActionResult<List<SubmissionData>> GetSubmissionsToCheck(string userid, string exerciseId)
+        public ActionResult<List<SubmissionLabel>> GetSubmissionsToCheck(string userid, string exerciseId)
         {
-            return FakeDatabase.Submissions;
+            return new StatusCodeResult(501);
         }
         [HttpGet]
         [Route("Checker/SubmissionToCheckList")]
-        public ActionResult<List<SubmissionData>> GetCheckedSubmissions(string userid, string exerciseId)
+        public ActionResult<List<Submission>> GetCheckedSubmissions(string userid, string exerciseId)
         {
-            return FakeDatabase.Submissions;
+            return new StatusCodeResult(501);
         }
         [HttpGet]
         [Route("Checker/SubmissionListToReCheck")]
-        public ActionResult<List<SubmissionData>> GetAppealSubmissions(string userid, string exerciseId)
+        public ActionResult<List<Submission>> GetAppealSubmissions(string userid, string exerciseId)
         {
-            return FakeDatabase.Submissions;
+            return new StatusCodeResult(501);
         }
-
         [HttpPost]
         [Route("Student/ValidateSubmitters")]
         public ActionResult<Dictionary<string, bool>> Validate(string userid, string exerciseId, [FromBody] List<string> studentIdList)
@@ -145,39 +139,62 @@ namespace Submit_System.Controllers
         }
         [HttpGet]
         [Route("Student/GetFile")]
-        public ActionResult GetFile(string userid, string submissionId, string file)
+        public ActionResult<SubmitFile> GetFile(string userid, string submissionId, string file)
         {
-            string ct;
-            string submitDirectory = _access.GetDirectory(userid, submissionId);
+            (string submitDirectory, DBCode code) = _access.GetSubmissionDirectory(userid, submissionId, Role.Student);
             if(submitDirectory == null)
             {
-                return NotFound("Submisison not found");
+                return HandleDatabaseOutput(code);
             }
-            string fullPath;
-            if(!FileUtils.TryGetValidPath(file, submitDirectory, out fullPath))
-            {
-                return BadRequest();
-            }
-            var IsTypeKnown = new FileExtensionContentTypeProvider().TryGetContentType(file, out ct);
-            if(!IsTypeKnown)
-            {
-                ct = "application/octet-stream";
-            }
-            byte[] res = System.IO.File.ReadAllBytes(fullPath);
-            return File(res, ct);
+            return HandleFileSending(submitDirectory, file);
         }
         [HttpGet]
         [Route("Student/Download")]
-        public ActionResult Download(string userid, string submissionId)
+        public ActionResult<SubmitFile> Download(string userid, string submissionId)
         {
-            string file = _access.GetDirectory(userid, submissionId);
+            (string file, DBCode code) = _access.GetSubmissionDirectory(userid, submissionId, Role.Student);
             if(file == null)
             {
-                return NotFound("Submission not found");
+                return HandleDatabaseOutput(code);
             }
-            var archive = FileUtils.ToArchiveBytes(file);
-            return File(archive, "application/zip", "ex.zip");
+            var archiveBytes = FileUtils.ToArchiveBytes(file);
+            if(archiveBytes == null)
+            {
+                return NotFound();
+            }
+            return SubmitFile.Create("ex.zip", archiveBytes);
+        }
+         [HttpPost]
+        [Route("Teacher/RejectRequest")]
+        public ActionResult<SubmitFile> CloseChat(string userid, string chatid)
+        {
+            if(chatid == null)
+            {
+                return BadRequest();
+            }
+            return HandleDatabaseOutput(_access.CloseChat(userid, chatid));
+        }
+        [HttpPost]
+        [Route("Teacher/CopyCheck")]
+        public ActionResult<string> CopyCheck(string userid, [FromBody] MossData data)
+        {
+            if(data == null)
+            {
+                return BadRequest();
+            }
+            (string folder, DBCode code) = _access.GetExerciseDirectory(data.ExerciseID, userid);
+            string sub = Path.Combine(folder, SUBMISSIONS);
+            string baseFiles = Path.Combine(folder, BASE_FILES);
+            Directory.CreateDirectory(baseFiles);
+            data.SubmissionsFolder =  sub;
+            data.BaseFilesFolder =  baseFiles;
+            if(code != DBCode.OK)
+            {
+                return HandleDatabaseOutput(code);
+            }
+            data.Result = _client.SendRequest(data);
+            _access.AddMossCheck(data);
+            return data.Result;
         }
     }
 }
-
