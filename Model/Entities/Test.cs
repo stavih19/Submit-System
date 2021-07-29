952b805e3e806c5337d7aedc9a956f7f98b16042
napan11 <46086872+napan11@users.noreply.github.com>
2021-07-29 22:03:49 +0300

    test update

--- Model/Entities/Test.cs
+++ Model/Entities/Test.cs
@@ -6,54 +6,45 @@ namespace Submit_System {
         public Test(){
             this.Value = 100;
             this.Input = "";
-            this.Expected_Output = "";
-            this.Output_File_Name = "stdout";
+            this.ExpectedOutput = "";
+            this.OutputFileName = "stdout";
             this.ArgumentsString= "";
-            this.Timeout_In_Seconds = 300;
-            this.Main_Sourse_File = "";
+            this.TimeoutInSeconds = 300;
+            this.MainSourseFile = "";
             this.AdittionalFilesLocation = "";
 
         }
 
-        public Test(int value,string input,string expected_output,string output_file_name,string arguments_string,int timeout_in_seconds,string main_sourse_file,string adittional_files_location,string exercise_id,int type){
+        public Test(int id,int value,string input,string expected_output,string output_file_name,string arguments_string,int timeout_in_seconds,string main_sourse_file,string adittional_files_location,string exercise_id,int type){
+            this.ID = id;
             this.Value = value;
             this.Input = input;
-            this.Expected_Output = expected_output;
-            this.Output_File_Name =output_file_name;
+            this.ExpectedOutput = expected_output;
+            this.OutputFileName =output_file_name;
             this.ArgumentsString = arguments_string;
-            this.Timeout_In_Seconds = timeout_in_seconds;
-            this.Main_Sourse_File = main_sourse_file;
+            this.TimeoutInSeconds = timeout_in_seconds;
+            this.MainSourseFile = main_sourse_file;
             this.AdittionalFilesLocation = adittional_files_location;
-            this.Exercise_ID = exercise_id;
+            this.ExerciseID = exercise_id;
             this.Type = type;
         }
-
+        public int ID {get; set;}
         public int Type{get;set;}
-        public string Exercise_ID{get;set;}
+        public string ExerciseID{get;set;}
         public int Value{get;set;}
         public string Input{get;set;}
-        public string Expected_Output{get;set;}
-        public string Output_File_Name{get;set;}
+        public string ExpectedOutput{get;set;}
+        public string OutputFileName{get;set;}
 
         public string ArgumentsString{get;set;}
 
-        public int Timeout_In_Seconds{get;set;}
+        public int TimeoutInSeconds{get;set;}
 
-        public string Main_Sourse_File{get;set;}
-
-        private string loc;
-        [JsonIgnore]
-        public string AdittionalFilesLocation{
-            get => loc;
-            set
-            {
-                AdditionFiles = FileUtils.GetRelativePaths(value);
-                loc = value;
-            }
-        }
+        public string MainSourseFile{get;set;}
+        public string AdittionalFilesLocation { get; set; }
         
-        public List<string> AdditionFiles {get; private set;}
-
+        public List<string> AdditionFileNames {get; set; }
+        public List<SubmitFile> AdditionalFiles { get; set; } = null; 
 
         public bool Has_Adittional_Files{get{return AdittionalFilesLocation != "";}}
     }
