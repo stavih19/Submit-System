63ba9b03b9b98566627f65e3bed1a23ee0fda4fb
napan11 <46086872+napan11@users.noreply.github.com>
2021-07-29 19:35:27 +0300

    from back-end

--- Program.cs
+++ Program.cs
@@ -1,29 +1,20 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System.Diagnostics;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System.Diagnostics;
-using Newtonsoft.Json;
+using System.Text;
 using System.IO;
+using System.Threading;
+using Microsoft.Extensions.Caching.Memory;
 namespace Submit_System
 {
     public class Program
     {
-
         public static void Main(string[] args)
         {
-            //Trace.WriteLine(CryptoUtils.Hash("password"));
-            //CryptoUtils.Test();
-            // var a = File.ReadAllBytes("ex2.zip");
-            // var b = Convert.ToBase64String(a);
-            // Trace.WriteLine(b);
             CreateHostBuilder(args).Build().Run();
         }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
